5f6da264f15a5cd6de1ace82546109166a9a8231
Sven Vowe <svenvowe@gmail.com>
2018-09-23 00:39:00 +0200

    working deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
-rsync -azP --exclude=.git --exclude=.gitignore --exclude=`git -C src ls-files --exclude-standard -oi --directory` ./src/ root@hobel:/var/www/rsynctest
+rsync -azP --exclude=.git --exclude=.gitignore --exclude=`git -C src ls-files --exclude-standard -oi --directory` ./src/ root@hobel:/var/www/bubakf
 
 echo 'Deployment complete'
