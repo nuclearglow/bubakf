0649f2d11af2d8d85cc53d825ef0ecc1572c4ecb
Sven Vowe <svenvowe@gmail.com>
2018-09-23 00:34:47 +0200

    deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,3 @@
-# rsync -avz --filter=':- .gitignore' . root@hobel:/var/www/rsynctest
 rsync -azP --exclude=.git --exclude=.gitignore --exclude=`git -C src ls-files --exclude-standard -oi --directory` ./src/ root@hobel:/var/www/rsynctest
 
 echo 'Deployment complete'
